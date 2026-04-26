2a2665e9caf68fbfc274a803f4bef0f1a8fc1113
Onke-dev <nadyushka.nats@gmail.com>
2026-04-26 17:56:03 +0300

    add modern normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App/App.tsx'
+// Нормалізація стилів
+import "modern-normalize";
+// Глобальні стилі (додатково)
+import "./global.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
