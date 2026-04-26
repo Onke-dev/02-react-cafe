66408f3e7fdd0efb8166647f5f25a17f3d6d4728
Onke-dev <nadyushka.nats@gmail.com>
2026-04-26 18:00:08 +0300

    INIT UPDATE

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App/App.tsx'
 // Нормалізація стилів
 import "modern-normalize";
-// Глобальні стилі (додатково)
-import "./global.css";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
